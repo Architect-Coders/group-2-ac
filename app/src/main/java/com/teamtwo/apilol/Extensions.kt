8bf56abe8686713395b4be52176530b52f418c73
Dani <K:htNFr-ah24xdkw>
2019-11-28 12:29:45 +0100

    Add example extensions

---
+++ app/src/main/java/com/teamtwo/apilol/Extensions.kt
@@ -0,0 +1,27 @@
+package com.teamtwo.apilol
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+fun Context.toast(message: String, lenght: Int = android.widget.Toast.LENGTH_LONG){
+    Toast.makeText(this, message, lenght).show()
+}
+
+fun RecyclerView.ViewHolder.toast(message: String, lenght: Int = Toast.LENGTH_LONG) {
+    itemView.context.toast(message, lenght)
+}
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+}
+
+fun ImageView.loadUrl(url: String) {
+    Picasso.with(context).load(url).into(this)
+}
