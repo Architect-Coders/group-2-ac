ff7f71951bdab7210b5a668e01e936ce33f5fdcc
Carlos Piña <carlos.p.m.2293@gmail.com>
2019-11-29 09:17:56 +0100

    - Added LOLServiceManager and LOLService with Architect Codders structure example in a comment.

---
+++ app/src/main/java/com/teamtwo/apilol/model/LOLService.kt
@@ -0,0 +1,16 @@
+package com.teamtwo.apilol.model
+
+interface LOLService {
+
+    /* Architect Codders Example
+
+    @GET("discover/movie?sort_by=popularity.desc")
+    fun listPopularMoviesAsync(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String
+    ): Deferred<MovieDbResult>
+
+    */
+
+
+}
---
+++ app/src/main/java/com/teamtwo/apilol/model/LOLServiceManager.kt
@@ -0,0 +1,24 @@
+package com.teamtwo.apilol.model
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class LOLServiceManager {
+
+    private val okHttpClient = HttpLoggingInterceptor().run {
+        level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder().addInterceptor(this).build()
+    }
+
+    val service: LOLService = Retrofit.Builder()
+        .baseUrl("http://ddragon.leagueoflegends.com/cdn/9.23.1/")
+        .client(okHttpClient)
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build().run {
+            create<LOLService>(LOLService::class.java)
+        }
+}
