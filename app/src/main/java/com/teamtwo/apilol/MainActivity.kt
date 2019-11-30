d8e895dfacd2fb5e617dce56553205f384ef0d7a
Jorge <jroldanreal@gmail.com>
2019-11-30 14:19:21 +0100

    Created basic implementation for BaseActivity and BaseFragment

--- app/src/main/java/com/teamtwo/apilol/MainActivity.kt
+++ app/src/main/java/com/teamtwo/apilol/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.teamtwo.apilol
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
