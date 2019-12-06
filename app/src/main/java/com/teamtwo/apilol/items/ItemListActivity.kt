74e97104a59efd412a9cd54e667c0225989318a4
dprdeveloper <davidpozorojas@gmail.com>
2019-12-06 14:18:40 +0100

    Create package from items with list and detail activity

---
+++ app/src/main/java/com/teamtwo/apilol/items/ItemListActivity.kt
@@ -0,0 +1,13 @@
+package com.teamtwo.apilol.items
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamtwo.apilol.R
+
+class ItemListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_list)
+    }
+}
