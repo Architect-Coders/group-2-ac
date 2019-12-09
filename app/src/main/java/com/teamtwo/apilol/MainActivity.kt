a2400629df69a96c5750e32a6ad75350a2688767
DprDeveloper <davidpozorojas@gmail.com>
2019-12-09 13:16:01 +0100

    Organize and initialize GpsLocation

--- app/src/main/java/com/teamtwo/apilol/MainActivity.kt
+++ app/src/main/java/com/teamtwo/apilol/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.teamtwo.apilol
 
+import android.content.Intent
 import android.os.Bundle
+import com.teamtwo.apilol.Sensor.CountryCode
+import com.teamtwo.apilol.Sensor.GpsLocation
+import com.teamtwo.apilol.items.ItemListActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        GpsLocation(this)
     }
 
     override fun initListeners() {
         btnCarlos.setOnClickListener { toast("Carlos") }
         btnDaniel.setOnClickListener { toast("Daniel") }
-        btnDavid.setOnClickListener { toast("David") }
+        btnDavid.setOnClickListener {
+            startActivity(Intent(this,ItemListActivity::class.java))
+        }
         btnJorge.setOnClickListener { toast("Jorge") }
         btnMary.setOnClickListener { toast("Mary") }
     }
--- app/src/main/java/com/teamtwo/apilol/Sensor/GpsLocation.kt
+++ app/src/main/java/com/teamtwo/apilol/Sensor/GpsLocation.kt
@@ -1,63 +1,41 @@
 package com.teamtwo.apilol.Sensor
 
-import android.icu.util.ULocale
-import android.location.Geocoder
+import android.app.Activity
+import android.content.Context
+import android.content.Context.LOCATION_SERVICE
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
 import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
-import com.teamtwo.apilol.R
-import com.teamtwo.apilol.toast
-import java.util.*
 
-
-class GpsLocation : AppCompatActivity() {
+class GpsLocation(val context: Context) : LocationListener {
 
     companion object {
-        var currentLocationcode: String = "en_US"
+        lateinit var location: Location
     }
+    private var locationManager = context.getSystemService(LOCATION_SERVICE) as LocationManager
 
-    private var locationManager : LocationManager? = null
-
-    private val locationListener: LocationListener = object : LocationListener {
-        override fun onLocationChanged(location: Location) {
-
-            val newLocation: String = getLocationCode(location)
-            locationSave(newLocation)
-        }
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-        override fun onProviderEnabled(provider: String) {}
-        override fun onProviderDisabled(provider: String) {}
-    }
-
-     fun initGps() {
-
-        locationManager = baseContext.getSystemService(LOCATION_SERVICE) as LocationManager?
-        checkPermission()
-    }
+    init { checkPermission() }
 
     private fun checkPermission(){
 
-        Dexter.withActivity(this)
+        Dexter.withActivity(context as Activity?)
             .withPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
             .withListener(object: PermissionListener{
                 override fun onPermissionGranted(response: PermissionGrantedResponse?) {
 
                     try {
-                        locationManager?.requestLocationUpdates(
+                        locationManager.requestLocationUpdates(
                             LocationManager.GPS_PROVIDER,
                             0L,
                             0f,
-                            locationListener
+                            this@GpsLocation
                         )
                     } catch(ex: SecurityException) {
 
@@ -76,30 +54,19 @@ class GpsLocation : AppCompatActivity() {
             }).check()
     }
 
-    private fun getLocationCode(location: Location): String{
-
-        var countryCode = getCountryCode(location)
-        var languageCode = getLanguageCode(countryCode)
-
-        return "${languageCode}_${countryCode}"
+    override fun onLocationChanged(currentLocation: Location) {
+        location = currentLocation
     }
 
-    private fun getCountryCode(location: Location): String{
-
-        return Geocoder(this)
-            .getFromLocation(
-                location.latitude,
-                location.longitude,
-                1)[0]
-            .countryCode
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    private fun getLanguageCode(countryCode: String): String{
-
-        return CountryLanguage.valueOf(countryCode).code
+    override fun onProviderEnabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    private fun locationSave(location:String){
-        currentLocationcode = location
+    override fun onProviderDisabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
