c7b11dfaef5502a4f9596a9173d01a8937e273de
DprDeveloper <davidpozorojas@gmail.com>
2019-12-10 21:34:11 +0100

    get country code with location gps

---
+++ app/src/main/java/com/teamtwo/apilol/sensor/CountryCode.kt
@@ -0,0 +1,18 @@
+package com.teamtwo.apilol.sensor
+
+import android.content.Context
+import android.location.Geocoder
+import android.location.Location
+
+class CountryCode(val location: Location, val context: Context) {
+
+    fun getCode(): String{
+        val countryData = Geocoder(context)
+            .getFromLocation(
+                location.latitude,
+                location.longitude,
+                1)[0]
+
+        return CountryLanguage.valueOf(countryData.countryCode).code
+    }
+}
