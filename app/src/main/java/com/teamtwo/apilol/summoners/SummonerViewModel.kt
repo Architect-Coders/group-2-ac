6cb8591a38b47ec9269b67c0c176a00ba6a81875
Carlos Piña <carlos.p.m.2293@gmail.com>
2019-12-28 12:06:53 +0100

    - Use of viewModelScope

--- app/src/main/java/com/teamtwo/apilol/summoners/SummonerViewModel.kt
+++ app/src/main/java/com/teamtwo/apilol/summoners/SummonerViewModel.kt
@@ -1,16 +1,11 @@
 package com.teamtwo.apilol.summoners
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.teamtwo.apilol.Scope
+import androidx.lifecycle.*
 import com.teamtwo.apilol.model.SummonerRepository
 import com.teamtwo.apilol.model.sumonners.Summoner
 import kotlinx.coroutines.launch
 
-class SummonnerViewModel(private val summonerRepository: SummonerRepository) : ViewModel(), Scope by Scope.Impl() {
+class SummonnerViewModel(private val summonerRepository: SummonerRepository) : ViewModel() {
 
     sealed class UiModelSummoner {
         object Loading : UiModelSummoner()
@@ -18,16 +13,13 @@ class SummonnerViewModel(private val summonerRepository: SummonerRepository) : V
         class Content(val summoner: Summoner): UiModelSummoner()
     }
 
-    init {
-        initScope()
-    }
 
     private val _model = MutableLiveData<UiModelSummoner>()
     val model: LiveData<UiModelSummoner>
         get() =  _model
 
     fun reload(name: String) {
-        launch {
+        viewModelScope.launch {
             _model.value = UiModelSummoner.Loading
             val championsResponse = summonerRepository.getSummoner(name).body()
             championsResponse?.let {
@@ -38,10 +30,6 @@ class SummonnerViewModel(private val summonerRepository: SummonerRepository) : V
         }
     }
 
-    override fun onCleared() {
-        destroyScope()
-        super.onCleared()
-    }
 }
 
 class SumonnerViewModelFactory(private val repository: SummonerRepository) : ViewModelProvider.Factory {
