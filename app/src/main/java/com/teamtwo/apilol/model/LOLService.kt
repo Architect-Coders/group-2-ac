65d2e5bfb7a0999b744e29c01dc7ee2938e0b05c
Carlos Piña <carlos.p.m.2293@gmail.com>
2019-12-28 12:12:57 +0100

    Fix merge

--- app/src/main/java/com/teamtwo/apilol/model/LOLService.kt
+++ app/src/main/java/com/teamtwo/apilol/model/LOLService.kt
@@ -8,6 +8,7 @@ import com.teamtwo.apilol.model.sumonners.Summoner
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface LOLService {
