28b0f135a8058f40a10320d568929c885b60bc06
dprdeveloper <davidpozorojas@gmail.com>
2020-01-28 21:07:02 +0100

    Creación de entidades

--- app/src/main/java/com/teamtwo/apilol/model/database/ApiLolDatabase.kt
+++ app/src/main/java/com/teamtwo/apilol/model/database/ApiLolDatabase.kt
@@ -4,11 +4,15 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.teamtwo.apilol.model.database.entities.ChampionEntity
+import com.teamtwo.apilol.model.database.entities.items.GoldEntity
+import com.teamtwo.apilol.model.database.entities.items.ImageEntity
+import com.teamtwo.apilol.model.database.entities.items.ItemEntity
 
-@Database(entities = [ChampionEntity::class], version = 1)
+@Database(entities = [ChampionEntity::class,ItemEntity::class,ImageEntity::class,GoldEntity::class], version = 1)
 @TypeConverters(Converters::class)
 abstract class ApiLolDatabase : RoomDatabase() {
 
     abstract fun apiLolDao(): ApilolDAO
+    abstract fun itemDAO(): ItemDAO
 
 }
