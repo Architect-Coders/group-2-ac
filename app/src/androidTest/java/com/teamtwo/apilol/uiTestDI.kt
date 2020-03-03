0b136cf31503cfedb18581a04735de904c26944c
Carlos Piña <carlospi@ucm.es>
2020-03-03 09:05:11 +0100

    fix ApiLolDatabase dependency

--- app/src/androidTest/java/com/teamtwo/apilol/uiTestDI.kt
+++ app/src/androidTest/java/com/teamtwo/apilol/uiTestDI.kt
@@ -1,11 +1,14 @@
 package com.teamtwo.apilol
 
 import android.app.Application
+import androidx.room.Room
+import com.teamtwo.apilol.di.DataSourcesModule
 import com.teamtwo.apilol.di.champions.ChampionsAppModule
 import com.teamtwo.apilol.di.champions.ChampionsComponent
 import com.teamtwo.apilol.di.champions.ChampionsDataModule
 import com.teamtwo.apilol.di.champions.ChampionsUseCaseModule
 import com.teamtwo.apilol.model.LOLServiceManager
+import com.teamtwo.apilol.model.database.ApiLolDatabase
 import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
@@ -35,6 +38,13 @@ interface UiTestComponent : ChampionsComponent {
 
 @Module
 class UiTestServerModule {
+
+    @Provides @Singleton
+    fun databaseProvider(app: Application): ApiLolDatabase = Room.databaseBuilder(
+        app,
+        ApiLolDatabase::class.java,
+        "api_lol_db"
+    ).build()
 
     @Provides
     @Singleton
