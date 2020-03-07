0a612c416ac884a968a9d4477174a7a77a507ce2
Jorge <jroldanreal@gmail.com>
2020-03-07 19:34:41 +0100

    Override component initilization to use the ui test instance

--- app/src/androidTest/java/com/teamtwo/apilol/UiTestApiLolApplication.kt
+++ app/src/androidTest/java/com/teamtwo/apilol/UiTestApiLolApplication.kt
@@ -1,7 +1,11 @@
 package com.teamtwo.apilol
 
+import com.teamtwo.apilol.di.matches.MatchesComponent
+
 class UiTestApiLolApplication : ApiLolAplication() {
 
     override fun initChampionsComponent() = DaggerUiTestComponent.factory().create(this)
+
+    override fun initMatchesComponent() = DaggerMatchesUiTestComponent.factory().create(this)
 
 }
