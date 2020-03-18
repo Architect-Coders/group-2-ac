e73bd100c0b0d8cb711a654fcbfffcdef889e6bd
DprDeveloper <davidpozorojas@gmail.com>
2020-03-19 00:40:06 +0100

    Añadir ItemsUiTestComponent y modificar referencia de componentes

--- app/src/androidTest/java/com/teamtwo/apilol/UiTestApiLolApplication.kt
+++ app/src/androidTest/java/com/teamtwo/apilol/UiTestApiLolApplication.kt
@@ -1,9 +1,11 @@
 package com.teamtwo.apilol
 
+import com.teamtwo.apilol.di.items.ItemsComponent
+
 class UiTestApiLolApplication : ApiLolAplication() {
 
     override fun initChampionsComponent() = DaggerUiTestComponent.factory().create(this)
-    override fun initItemsComponent() = DaggerUiTestComponent.factory().create(this)
+    override fun initItemsComponent() = DaggerItemsUiTestComponent.factory().create(this)
     override fun initSpellsComponent() = DaggerSpellsUiTestComponent.factory().create(this)
     override fun initMatchesComponent() = DaggerMatchesUiTestComponent.factory().create(this)
 
